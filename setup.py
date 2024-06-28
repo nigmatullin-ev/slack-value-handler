6ab5f1ef0c0fa71127e82d0e90a672e4768bd4d4
Eduard Nigmatullin <nigmatullinev@pnlfin.tech>
2024-06-28 11:28:04 +0300

    impr: make it as lib

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="slack_value_handler",
+    version="1.0.0",
+    description="Value handler for slack",
+    author="Eduard Nigmatullin",
+    author_email="nigmatullin.ev@gmail.com",
+    packages=find_packages()
+)
